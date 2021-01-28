0d8c9971d0ec23120b341fa5d0acef17f6c0197f
InnervisionGit <ivan.felbabic@gmail.com>
2021-01-28 11:49:21 +0100

    Server

--- src/app/data-table/data-table-datasource.ts
+++ src/app/data-table/data-table-datasource.ts
@@ -4,7 +4,7 @@ import { MatSort } from '@angular/material/sort';
 import { map } from 'rxjs/operators';
 import { Observable, of as observableOf, merge } from 'rxjs';
 
-// TODO: Replace this with your own data model type
+// data model type
 export interface DataTableItem {
   id: number;
   name: string;
